87cf9e019febc9150ae8f0736a7dfd2ca694fe42
Roquelson <roquelsonferreira@assistevida.com>
2021-08-19 10:27:05 -0300

    Dia01_Exercicio04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,25 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+const app = document.querySelector('[data-js="app"]');
+      app.innerHTML = `
+           <h1>B. Academy</h1>
+           <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+      `
+const link = document.querySelector('[data-js="link"]')
+
+link.addEventListener('click', (e)=>{
+  e.preventDefault();
+  app.classList.contains('hidden') === true ?  show(): hide()
+})
+
+const show = () => {
+  app.classList.remove('hidden')
+  link.innerHTML='ESCONDER'
+}
+
+const hide = () => {
+  app.classList.add('hidden')
+  link.innerHTML='EXIBIR'
+}
+
+
