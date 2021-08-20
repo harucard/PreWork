7d11d384dc2f9f8d6295e500386e0b6961d1b8bc
Roquelson <roquelsonferreira@assistevida.com>
2021-08-20 11:44:48 -0300

    Dia03_Exercicio02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,21 +1,74 @@
+import './style.css'
+
 const input = document.querySelector('[data-js="name"]')
 const form = document.querySelector('[data-js="form"]')
-const prepositions = ["de","da","do","dos"]
+const select = document.createElement('select')
+const color_container = document.createElement('div')
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault()
-})
+const prepositions = ["de", "da", "do", "dos"]
+const options = [
+  {label:"Azul",value:"#0000FF"},
+  {label:"Amarelo",value:"#FFFF00"},
+  {label:"Verde",value:"#00FF00"},
+  {label:"Vermelho",value:"#FF0000"},
+  {label:"Marrom",value:"#964B00"}
+]
 
-input.addEventListener('input',(e)=>{
+select.setAttribute('multiple','')
+color_container.style.display = "flex"
+input.style.display = "block"
+input.style.marginBottom = "10px"
+
+//Exercicio 01
+input.addEventListener('input', (e) => {
   const stringArray = e.target.value.split(' ');
-  const captalizedString = stringArray.map((word)=> {
-    if(!prepositions.includes(word.toLowerCase())) {
-        return word.charAt(0).toUpperCase()+word.slice(1).toLowerCase() 
-    }else {
-        return word.charAt(0).toLowerCase()+word.slice(1).toLowerCase()
+  const captalizedString = stringArray.map((word) => {
+    if (!prepositions.includes(word.toLowerCase())) {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    } else {
+      return word.charAt(0).toLowerCase() + word.slice(1).toLowerCase()
     }
-        
+
   })
   e.target.value = captalizedString.join(' ')
+
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+})
+//Exercicio 02
+options.map((option)=>{
+  const select_option = document.createElement('option')
+  select_option.value = option.value
+  select_option.innerHTML = option.label
+  select.appendChild(select_option)
+})
+form.appendChild(select)
+
+select.addEventListener('change', (event)=> {
+  //removendo todas as div's para renderizar novamente
+  while(color_container.firstChild) {
+    color_container.removeChild(color_container.firstChild);
+  }
+  let getColors = [];
+  console.log(event.target.options)
+  
+  Array.from(event.target.options).map((option,index)=> {
+    if(option.selected)
+    getColors = [...getColors,option.value]
+  })
+  getColors.map((color)=> {
+    const square = document.createElement('div')
+    square.setAttribute('class','square')
+    square.style.backgroundColor = color
+    color_container.appendChild(square)
+  })
+  form.append(color_container)
   
 })
+
+
+
+
+
